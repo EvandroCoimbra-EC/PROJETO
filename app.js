68a854a1a6b0588f188bc1149272534d74d676dc
Jamir soares rodrigues <jamirmelo7@gmail.com>
2023-02-11 09:13:21 -0300

    alteração
    11-02-2023

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+const port = 8030
+app.use(express.static(__dirname+'/public'));
+
+
+app.get('/',function(req,res){
+    res.sendfile(__dirname + '/public/html/index.html')
+})
+
+app.listen(port,()=>{
+    console.log(`servidor rodando em localhost:${port}`)
+})
