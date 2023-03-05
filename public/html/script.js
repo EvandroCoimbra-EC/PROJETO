21c31f9c3b55ab64fd6af2ba29a964038cd58844
Evandro programação <110572176+EvandroCoimbra-EC@users.noreply.github.com>
2023-03-04 21:18:51 -0300

    04/03/2023
    atualização do projeto

---
+++ public/html/script.js
@@ -0,0 +1,17 @@
+var btn = document.querySelector('#show-or-hide');
+var container = document.querySelector('.container');
+
+btn.addEventListener('click', function(){
+
+    if(container.style.display === 'block') {
+        container.style.display = 'none';
+    } else {
+        container.style.display = 'block';
+    }
+}); 
+
+
+
+function onmouseenter(){
+     var a = window.document.getElementById("cor_botoes")
+}
