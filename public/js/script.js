655c465396039291043bf2a7ba8fd298d899a928
Evandro programação <110572176+EvandroCoimbra-EC@users.noreply.github.com>
2023-04-01 11:42:20 -0300

    ok

---
+++ public/js/script.js
@@ -0,0 +1,8 @@
+
+	var palavra = document.getElementById("palavra");
+	palavra.addEventListener("mouseover", function(){
+		palavra.style.color = "blue";
+	});
+	palavra.addEventListener("mouseout", function(){
+		palavra.style.color = "black";
+	});
